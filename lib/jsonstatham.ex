52155390cc06538ba2cdc33e4b2b23cdfc112c7a
Michael Pope <mpope.cr@gmail.com>
2016-10-26 17:41:44 -0700

    Add empty text error

--- lib/jsonstatham.ex
+++ lib/jsonstatham.ex
@@ -1,2 +1,9 @@
 defmodule JSONStatham do
+  @moduledoc """
+  A JSON parser written in pure Elixir
+  """
+
+  def parse("") do
+    {:error, :empty_string}
+  end
 end
--- test/jsonstatham_test.exs
+++ test/jsonstatham_test.exs
@@ -1,8 +1,9 @@
 defmodule JSONStathamTest do
   use ExUnit.Case
-  doctest JSONStatham
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  describe "empty string" do
+    test "returns error" do
+      assert JSONStatham.parse("") == {:error, :empty_string}
+    end
   end
 end
